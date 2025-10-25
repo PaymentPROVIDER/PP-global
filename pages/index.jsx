0af56d5dd9fad49aa44e8926f8c342cfeb97b1ab
PaymentPROVIDER <office@eyeidglobal.com>
2025-10-25 19:54:10 +0200

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>PaymentProvider Global</h1>
+      <p>Witamy na stronie głównej!</p>
+    </div>
+  );
+}
