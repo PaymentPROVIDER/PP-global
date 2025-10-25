f4b1ed08dc73d07036e5920f25d2cefe05676392
PaymentPROVIDER <office@eyeidglobal.com>
2025-10-25 21:29:26 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,2 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>PaymentProvider Global</h1>
-      <p>Witamy na stronie głównej!</p>
-    </div>
-  );
-}
+import PaymentProviderSite from "../components/PaymentProviderSite";
+export default function Home() { return <PaymentProviderSite />; }
