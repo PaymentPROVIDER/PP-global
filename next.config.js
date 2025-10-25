f5425edc8ea7b1ec9a4bbbaff066c480f6efa8da
PaymentPROVIDER <office@eyeidglobal.com>
2025-10-25 20:03:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/PP-global',
-  assetPrefix: '/PP-global/',
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
+  images: {
+    unoptimized: true
+  },
+  reactStrictMode: true
 }
+
+export default nextConfig;
